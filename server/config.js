1514e4e81d15675343a42701376886a04f43cff5
mao-user <maoquyi@gmail.com>
2025-01-11 11:13:23 +0800

    1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置1/11/11:13:添加服务器信息和基础配置

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 //Https SSL 配置
-var privateKey = fs.readFileSync('*.key', 'utf8');
-var certificate = fs.readFileSync('*.crt', 'utf8');
+var privateKey = fs.readFileSync('server.key', 'utf8');
+var certificate = fs.readFileSync('server.crt', 'utf8');
 exports.credentials = {
 	key : privateKey, 
 	cert : certificate
